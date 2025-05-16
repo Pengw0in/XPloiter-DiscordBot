3b75c142f007ccdc4376179220ebc8ec5e5bd14a
Lohith Srikar <167197181+l-Srikar@users.noreply.github.com>
2025-05-16 23:54:55 +0530

    init commit

---
+++ main.py
@@ -0,0 +1,58 @@
+import os
+import discord, asyncio , aiohttp
+from datetime import datetime, timedelta, UTC
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv('BOT_TOKEN')
+CHANNEL = int(os.getenv('ID'))
+
+intents = discord.Intents.default()
+client = discord.Client(intents=intents)
+
+
+async def fetch_events():
+    now = int(datetime.now(UTC).timestamp())
+    future = int((datetime.now(UTC) + timedelta(days=2)).timestamp())
+    url = f"https://ctftime.org/api/v1/events/?limit=100&start={now}&finish={future}"
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as r:
+            return await r.json()
+
+
+async def daily_updates():
+    await client.wait_until_ready() 
+    channel = client.get_channel(CHANNEL)
+    while not client.is_closed():
+        ctfs = await fetch_events()
+        if ctfs:
+            for ctf in ctfs:
+                name = ctf['title']
+                org = ctf['organizers'][0]['name']
+                url_ctf = ctf['ctftime_url']
+                start = ctf['start'].split('T')[0]
+                end = ctf['finish'].split('T')[0]
+                logo = ctf.get('logo') or None
+                onsite = "On-site" if ctf.get('onsite') else 'Online'
+                description = ctf['description']
+
+                message = discord.Embed(title=name, url=url_ctf, description=description)
+                message.add_field(name='Organized by', value=org, inline=True)
+                message.add_field(name='Type', value=onsite, inline=True)
+                message.add_field(name='Starts on', value=start, inline=True)
+                message.add_field(name= 'Ends on', value=end, inline=True)
+                if logo:
+                    message.set_thumbnail(url=logo)
+                await channel.send(embed=message)
+        await asyncio.sleep(86400)
+
+@client.event
+async def on_ready():
+    print(f"we have logged in as {client.user}")
+
+
+client.loop.create_task(daily_updates())
+client.run(TOKEN)
+
+
+
